e22097455735d347d255b045b28ecca0679bde53
Matthew <kashin.developer@gmail.com>
2023-11-16 16:17:34 +0200

    create empty project with settings

---
+++ NewsArticles/NewsArticles/NewsArticlesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsArticlesApp.swift
+//  NewsArticles
+//
+//  Created by Матвей Кашин on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsArticlesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
