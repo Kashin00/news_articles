d9fdce88a20e83b00d3ad4dd5eef585f4e666877
Matthew <kashin.developer@gmail.com>
2023-11-18 00:59:46 +0200

    add texts

--- NewsArticles/NewsArticles/Modules/ArticleDetailsView.swift
+++ NewsArticles/NewsArticles/Modules/ArticleDetailsView.swift
@@ -12,9 +12,32 @@ struct ArticleDetailsView: View {
     var article: Article
     
     var body: some View {
-        VStack {
+        VStack(alignment: .leading, spacing: 8) {
+//            posterImage
             
-            posterImage
+            HStack(spacing: 0) {
+                Text("source")
+                Text(article.source.name)
+            }
+            .font(.subheadline)
+            
+            Text(article.title)
+                .font(.headline)
+            
+            Text(article.description ?? "")
+                .font(.body)
+            HStack(spacing: 0) {
+                Text("published_at")
+                Text(article.publishedAt)
+            }
+            .font(.subheadline)
+            
+            HStack(spacing: 0) {
+                Text("created_by")
+                Text(article.author ?? "")
+            }
+            .font(.subheadline)
+            
             Spacer()
         }
         .padding(EdgeInsets(top: 0, leading: 8, bottom: 0, trailing: 8))
@@ -27,6 +50,7 @@ struct ArticleDetailsView: View {
             GeometryReader { geometry in
                 imagePhase.image?
                     .resizable()
+                    .aspectRatio(contentMode: .fit)
                     .frame(width: geometry.size.width, height: geometry.size.height / 3, alignment: .center)
                     .cornerRadius(10)
             }
