222c5f054de326113c5168fc30166e08fc581f83
Matthew <kashin.developer@gmail.com>
2023-11-18 00:37:36 +0200

    add article details view

---
+++ NewsArticles/NewsArticles/Modules/ArticleDetailsView.swift
@@ -0,0 +1,20 @@
+//
+//  ArticleDetailsView.swift
+//  NewsArticles
+//
+//  Created by Матвей Кашин on 18.11.2023.
+//
+
+import SwiftUI
+
+struct ArticleDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleDetailsView()
+    }
+}
